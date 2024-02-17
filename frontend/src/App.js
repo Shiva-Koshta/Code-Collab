c2fa0b79313c65aaefe96f9d55e6f58ca8853f00
Shiva-Koshta <114857995+Shiva-Koshta@users.noreply.github.com>
2024-02-18 01:13:12 +0530

    resolved syntax errror
    Removed the reduntant "=" signs which are giving syntax error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-=======
 import Login from './pages/Login';
 function App() {
   return (
@@ -22,7 +21,6 @@ function App() {
     //   </header>
     // </div>
 
-=======
     <Login/>
   );
 }
