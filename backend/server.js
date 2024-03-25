4c83bdb0dd1803bc9f5c835aa3920c6eafd66032
Komalpreet Singh <2021csb1237@iitrpr.ac.in>
2024-03-25 22:45:53 +0530

    Devided the code of server into different modules for easy unserstanding

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,25 @@
 require('dotenv').config()
-const mongoose = require('mongoose')
 const express = require('express')
-const cors = require('cors')
-const passport = require('passport')
-const authRoute = require('./routes/auth')
-const cookieSession = require('cookie-session')
 // eslint-disable-next-line no-unused-vars
 const passportStrategy = require('./passport')
-const { Server } = require('socket.io')
 const http = require('http')
-const ACTIONS = require('../frontend/src/Actions')
+const mongoose = require('mongoose')
+const { Server } = require('socket.io')
+const endpoints = require('./endpoints')
+const middleware = require('./middleware')
 const RoomCodeMap = require('./models/RoomCodeMap')
 const RoomUserCount = require('./models/RoomUserCount')
+const ACTIONS = require('../frontend/src/Actions')
+
 const app = express()
 const server = http.createServer(app)
+const io = new Server(server)
 const port = process.env.PORT || 8080
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true
-  })
-)
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-// const roomCodeMap = {}
-
-const io = new Server(server)
-
-// might to needed to store it in redux or database
+app.use(middleware)
+app.use(endpoints)
 const userSocketMap = {}
-// mongoose
-//   .connect(process.env.MONGO_URL)
-//   .then(() => {
-//     console.log("connected to database");
-//     server.listen(port, () => console.log(`Listenting on port ${port}...`));
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-// this function could be shifted to some other file where all similar functions are written
+
 function getAllConnectedClients (roomId) {
   return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
     (socketId) => {
@@ -51,7 +30,7 @@ function getAllConnectedClients (roomId) {
     }
   )
 }
-
+// Define socket.io logic
 io.on('connection', (socket) => {
   console.log('Socket connected', socket.id)
 
@@ -157,97 +136,13 @@ io.on('connection', (socket) => {
   })
 })
 
-// app.use(function (req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   next();
-// });
-// app.use(cors({ origin: "*" }));
-
-app.use(
-  cookieSession({
-    name: 'session',
-    keys: ['cyberwolve'],
-    maxAge: 24 * 60 * 60 * 100
-  })
-)
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-app.use('/auth', authRoute)
-app.post('/receivecode', (req, res) => {
-  const { roomId } = req.body
-
-  // Retrieve the code from the database
-  RoomCodeMap.findOne({ roomId })
-    .then((roomMap) => {
-      if (roomMap) {
-        res.json({ code: roomMap.code })
-      } else {
-        res
-          .status(404)
-          .json({ error: 'Code not found for the specified room' })
-      }
-    })
-    .catch((error) => {
-      console.error('Error retrieving code from database:', error)
-      res.status(500).json({ error: 'Internal server error' })
-    })
-})
-// to handle post request from help page
-app.post('/help', (req, res) => {
-  console.log(req.body)
-  res.status(200).json({ message: 'Form submitted' })
-})
-
-// check and delete the room data if no user in the room
-app.post('/delete-entry', async (req, res) => {
-  // console.log("hit");
-  const { roomId } = req.body
-
-  try {
-    // Check if the user count for the room is zero
-    const room = await RoomUserCount.findOne({ roomId })
-
-    if (!room) {
-      return res.status(200).json({ message: 'Room not found' })
-    }
-
-    if (room.userCount !== 0) {
-      return res.status(200).json({ message: 'done' })
-    }
-
-    // If user count is zero, delete the entry from RoomCodeMap
-    const deletedRoomCodeMap = await RoomCodeMap.findOneAndDelete({ roomId })
-
-    if (!deletedRoomCodeMap) {
-      return res.status(404).json({ error: 'Room code map entry not found' })
-    }
-
-    return res.json({ message: 'Room code map entry deleted successfully' })
-  } catch (error) {
-    console.error('Error deleting room code map entry:', error)
-    return res.status(500).json({ error: 'Internal server error' })
-  }
-})
-// connect to database
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log('connected to database')
-    // server.listen(port, () => console.log(`Listenting on port ${port}...`));
+    console.log('Connected to database')
+    server.listen(port, () => console.log(`Listening on port ${port}...`))
   })
   .catch((error) => {
-    console.log(error)
+    console.error('Error connecting to database:', error)
   })
-
-server.listen(port, () => console.log(`Listenting on port ${port}...`))
