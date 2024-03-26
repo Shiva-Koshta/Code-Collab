0c6b85ad645d19f8715eec2c97c50c365c51a212
Shashank Kumar <skumar18nams@gmail.com>
2024-03-26 16:31:29 +0530

    improve

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,13 +1,25 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+const reportWebVitals = (onPerfEntry) => {
+  if (typeof onPerfEntry !== 'function') {
+    console.warn('onPerfEntry must be a function');
+    return;
   }
+
+  import('web-vitals')
+    .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      const report = (metric) => {
+        onPerfEntry(metric);
+      };
+
+      if (getCLS) getCLS(report);
+      if (getFID) getFID(report);
+      if (getFCP) getFCP(report);
+      if (getLCP) getLCP(report);
+      if (getTTFB) getTTFB(report);
+    })
+    .catch((error) => {
+      console.error('Error importing web-vitals module:', error);
+    });
 };
 
 export default reportWebVitals;
+
