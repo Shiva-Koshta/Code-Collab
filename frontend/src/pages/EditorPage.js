2d69fd16e47bac80d78002a3747c664ff848d028
Shashank Kumar <skumar18nams@gmail.com>
2024-03-26 16:31:31 +0530

    improve

--- frontend/src/pages/EditorPage.js
+++ frontend/src/pages/EditorPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Navigate, useLocation, useNavigate, useParams } from "react-router-dom";
-// import "./Editor.css";
 import ACTIONS from "../Actions";
 import toast from 'react-hot-toast';
 import Editor from "../components/Editor";
@@ -15,20 +14,17 @@ const EditorPage = () => {
   const { roomId } = useParams();
   const socketRef = useRef(null);
   const location = useLocation();
-  const reactNavigator = useNavigate();
+  const navigate = useNavigate();
   const [clients, setClients] = useState([]);
-  const [storedUserData, setStoredUserData] = useState([]);
+  const [storedUserData, setStoredUserData] = useState(null);
   const [connectedUsernames, setConnectedUsernames] = useState([]);
   const [messages, setMessages] = useState([]);
   const [inputText, setInputText] = useState('');
   const [fileContent, setFileContent] = useState("");
-  // const fileRef=useRef(null);
-  const [isOpen, setIsOpen] = useState(true);
-  // const [isOpen, setIsOpen] = useState(true);
   const [isChatOpen, setIsChatOpen] = useState(false); // State to control chat window
   const [contentChanged, setContentChanged] = useState(false);
+
   const handleMessageSend = () => {
-    console.log(storedUserData);
     if (inputText.trim() !== '') {
       const message = { text: inputText };
       socketRef.current.emit(ACTIONS.MESSAGE_SEND, { roomId, message, sender: storedUserData.sub, sendname: storedUserData.name });
@@ -41,28 +37,24 @@ const EditorPage = () => {
   };
 
   const leaveRoom = () => {
-    console.log("in LeaveRoom")
-    reactNavigator('/', {
-      roomId: roomId,
+    navigate('/', {
+      state: { roomId },
     });
   };
+
   const handleFileChange = (event) => {
-    console.log("reached");
     const file = event.target.files[0];
     const reader = new FileReader();
-      setContentChanged(!contentChanged);
-      // console.log(contentChanged);
+    setContentChanged(!contentChanged);
     reader.onload = (e) => {
-     const content = e.target.result;
+      const content = e.target.result;
       setFileContent(content);
-      // console.log(content);
-      // fileRef.current = content;
     };
     if(file)
     {reader.readAsText(file);}
-    // console.log("fileref here:",fileContent);
     event.target.value = null;
   };
+
   useEffect(() => {
     const init = async () => {
       socketRef.current = await initSocket();
@@ -72,17 +64,18 @@ const EditorPage = () => {
       function handleErrors(e) {
         console.log('socket error', e);
         toast.error('Socket connection failed, try again later.');
-        reactNavigator('/');
+        navigate('/');
       }
+
       const userData = localStorage.getItem("userData");
       if (userData) {
-        console.log(JSON.parse(userData).name);
         setStoredUserData(JSON.parse(userData));
         socketRef.current.emit(ACTIONS.JOIN, {
           roomId,
           username: JSON.parse(userData).name,
         });
       }
+
       socketRef.current.on(ACTIONS.JOINED, ({ clients, username, socketId }) => {
         if (socketId !== socketRef.current.id) {
           toast.success(
@@ -111,13 +104,11 @@ const EditorPage = () => {
           return updatedClients;
         });
       });
+
       socketRef.current.on(ACTIONS.MESSAGE_RECEIVE, ({ text, sender, sendname }) => {
         console.log(`${sender}: ${text}`);
-        console.log(storedUserData);
-        console.log(userData);
-        setMessages(prev => [...prev, { text, sender, sentByCurrentUser: sender === JSON.parse(userData).sub, sendname }]);
-      });      
-
+        setMessages(prev => [...prev, { text, sender, sentByCurrentUser: sender === storedUserData.sub, sendname }]);
+      });
     };
 
     init();
@@ -128,7 +119,6 @@ const EditorPage = () => {
       }
     };
   }, [roomId]);
-
 
   if (!location.state) {
     return <Navigate to="/" />;
@@ -159,89 +149,4 @@ const EditorPage = () => {
         style={{backgroundColor: "#1c1e29"}}
       >
         <div className="logo flex gap-6">
-          <img className="h-24" src={logo} alt="logo" />
-          <div className="flex flex-col justify-center">
-            <h2 className="text-4xl madimi-one-regular">Code Collab</h2>
-          </div>
-        </div>
-        <div className="flex flex-col justify-between h-full">
-          <div className="fileTreeView">
-            <p className="my-3 text-lg font-bold" for="file_input">
-              Upload file
-            </p>
-            <input
-              className="mb-3"
-              style={{color: "#1c1e29"}}
-              id="file_input"
-              type="file"
-              onChange={handleFileChange}
-            />
-
-            <FileView></FileView>
-          </div>
-
-          <div className="Users">
-            <h3 className="my-3 font-bold text-lg">Connected Users here</h3>
-            {connectedUsernames.map(username => (
-              <div className="UserList" key={username}>{username}</div>
-            ))}
-          </div>
-        </div>
-        <div>
-          <button className="btn chatBtn" onClick={toggleChat} >Chat</button>
-          <button className="btn-edit copyBtn" onClick={copyRoomId}>Copy ROOM ID</button>
-          <button className="btn-edit leaveBtn" onClick={leaveRoom}>
-            Leave
-          </button>
-        </div>
-      </div>
-
-       
-      <div className={"overflow-y-auto"} style={{ width: isChatOpen ? `calc(100% - 300px)` : '100%' }}>
-        <Editor
-          fileContent={fileContent}
-          socketRef={socketRef}
-          roomId={roomId}
-          contentChanged={contentChanged}
-          // cursorInfoList={cursorInfoList}
-        />
-      </div>
-
-      {isChatOpen && (
-        <div className="chat-container">
-          <div className="chat-popup">
-            <div className="chat-header">
-              <h2>Chat</h2>
-              <button className="close-icon" onClick={() => setIsChatOpen(false)}>X</button>
-            </div>
-            <div className="chat-messages">
-              {messages.map((message, index) => (
-                <div key={index} className={` ${message.sentByCurrentUser ? 'sent_by_user' : 'chat-message'}`}>
-                  <span className="message-sender">{message.sentByCurrentUser ? 'You' : message.sendname}:</span> {message.text}
-                </div>
-              ))}
-            </div>
-            <div className="chat-input">
-              <input
-                type="text"
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
-                onKeyPress={handleKeyPress}
-                placeholder="Type your message..."
-                className="input-field"
-              />
-              <button onClick={handleMessageSend} className="send-button">Send</button>
-            </div>
-          </div>
-        </div>
-      )}
-
-
-
-
-    </div>
-
-  );
-};
-
-export default EditorPage;
+         
