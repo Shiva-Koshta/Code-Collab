9bcba87b3ca5573de371341b112db644a05f9d3c
Shashank Kumar <skumar18nams@gmail.com>
2024-03-26 16:31:25 +0530

    improve

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,24 +1,29 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import roomSlice from "./roomSlice";
 import userSlice from "./userSlice";
-import userReducer from "./userSlice";
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
-  user: userReducer
+  user: userSlice, // use userSlice directly instead of importing userReducer separately
+  room: roomSlice
 });
 
 const persistConfig = {
   key: 'root',
   storage,
   version: 1,
-}
+  whitelist: ['user'] // only the user state will be persisted
+};
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // disable serializable check warning
+    }),
 });
 
 export const persistor = persistStore(store);
