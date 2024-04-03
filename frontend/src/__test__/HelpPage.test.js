9a5fba245ed89b450afe77931a701047313d9ad0
Anisha_Prakash <anishaprakashmbec@gmail.com>
2024-04-03 18:27:44 +0530

    setup

---
+++ frontend/src/__test__/HelpPage.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import HelpPage from '../pages/HelpPage'; // Assuming this is your HelpPage component
+
+
