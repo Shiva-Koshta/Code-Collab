d188af41401b2a7b5e67d3faa406e687f08f7664
Vanshika-Dhamija <vanshikad2002@gmail.com>
2024-04-07 17:36:22 +0530

    file icons

--- frontend/src/components/FileView.js
+++ frontend/src/components/FileView.js
@@ -16,6 +16,26 @@ import ArrowRightIcon from '@mui/icons-material/ArrowRight'
 import { FolderCopy } from '@mui/icons-material'
 import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
 import axios from 'axios'
+// import ImageIcon from '@mui/icons-material/Image'; // Image File
+// import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf'; // PDF File
+// import AudiotrackIcon from '@mui/icons-material/Audiotrack'; // Audio File
+// import MovieIcon from '@mui/icons-material/Movie'; // Video File
+import audioIcon from '../icons/audio.png';
+import cIcon from '../icons/c.png';
+import cppIcon from '../icons/cpp.png';
+import csharpIcon from '../icons/cs.png';
+import cssIcon from '../icons/css.png';
+import defaultIcon from '../icons/default.png';
+import htmlIcon from '../icons/html.png';
+import imageIcon from '../icons/image.png';
+import jsIcon from '../icons/js.png';
+import jsonIcon from '../icons/json.png';
+import pdfIcon from '../icons/pdf.png';
+import pythonIcon from '../icons/python.png';
+import textIcon from '../icons/text.png';
+import videoIcon from '../icons/video.png';
+// Import other file type icons as needed
+
 
 const FileView = ({ fileContent, setFileContent, editorRef, contentChanged, setContentChanged }) => {
   const { roomId } = useParams()
@@ -268,7 +288,8 @@ const FileView = ({ fileContent, setFileContent, editorRef, contentChanged, setC
                   setSelectedFileFolder(folder)
                   setSelectedFileFolderParent(parentFolder)
                 }}>
-                <TextFileIcon className='mr-2 pb-0.5' style={{ fontSize: 20 }} />
+                {/* <TextFileIcon className='mr-2 pb-0.5' style={{ fontSize: 20 }} /> */}
+                {renderFileIcon(folder)}
                 <div className='truncate'>{folder.name}</div>
               </div>
             )}
@@ -278,6 +299,105 @@ const FileView = ({ fileContent, setFileContent, editorRef, contentChanged, setC
       </div>
     )
   }
+  // const fileIconMap = {
+  //       txt: <TextFileIcon className='mr-2 pb-0.5' style={{ fontSize: 20 }} />,
+  //       json: <ImageIcon/>,
+  //       py: <AudiotrackIcon />,
+  //       html: <ImageIcon />,
+  //       css: <TextFileIcon />,
+  //       java: <MovieIcon />,
+  //       cpp: <PictureAsPdfIcon />,
+  //       c: <PictureAsPdfIcon />,
+  //       js: <MovieIcon />,
+  //     jpg: <ImageIcon />, // Image File
+  //     pdf: <PictureAsPdfIcon />, // PDF File
+  //     mp3: <AudiotrackIcon />, // Audio File
+  //     mp4: <MovieIcon />, // Video File
+  //       // Add more mappings as needed
+  //     };
+  //     const getFileIcon = (extension) => {
+  //       return fileIconMap[extension] || <MovieIcon />;
+  //     };
+  //     const renderFileIcon = (file) => {
+  //       const extension = file.name.split('.').pop().toLowerCase();
+  //       const icon = getFileIcon(extension);
+  //       return <div className='file-icon'>{icon}</div>;
+  //     };
+
+
+
+  //const fileIconMap = {
+  //   txt: '../images/Logo.png',
+  //   json: '../images/Logo.png',
+  //   py: '../images/Logo.png',
+  //   html: '../images/Logo.png',
+  //   css: '../images/Logo.png',
+  //   java: '../images/Logo.png',
+  //   cpp: '../images/Logo.png',
+  //   c: '../images/Logo.png',
+  //   js: '../images/Logo.png',
+  // jpg: '../images/Logo.png', // Image File
+  // pdf: '../images/Logo.png', // PDF File
+  // mp3: '../images/Logo.png', // Audio File
+  // mp4: '../images/Logo.png', // Video File
+  // default: '..images/Logo.png',
+  //};
+  // const getFileIcon = (extension) => {
+  //   // Check if extension exists in the icon map, otherwise return default icon
+  //   return fileIconMap[extension.toLowerCase()] || fileIconMap['default'];
+  // };
+
+  // const renderFileIcon = (file) => {
+  //   // Extract extension from file name
+  //   const extension = (file.name.split('.').pop() || '').toLowerCase();
+  //   // Get corresponding icon URL
+  //   const iconUrl = getFileIcon(extension);
+  //   // Render icon
+  //   return (
+  //     <div className='file-icon'>
+  //       <img src={iconUrl} alt={`${extension} icon`} onError={(e) => {
+  //         // If the image fails to load, display a placeholder or fallback image
+  //         e.target.src = fileIconMap['default'];
+  //       }} />
+  //     </div>
+  //   );
+  // };
+
+  const fileIconMap = {
+    mp3: audioIcon,
+    c: cIcon,
+    cs: csharpIcon,
+    cpp: cppIcon,
+    css: cssIcon,
+    html: htmlIcon,
+    jpg: imageIcon,
+    jpeg: imageIcon,
+    png: imageIcon,
+    svg: imageIcon,
+    js: jsIcon,
+    json: jsonIcon,
+    pdf: pdfIcon,
+    py: pythonIcon,
+    txt: textIcon,
+    mp4: videoIcon,
+    // Add mappings for other file types with corresponding image files
+  };
+  const getFileIcon = (extension) => {
+    // Check if extension exists in the icon map, otherwise return default icon
+    return fileIconMap[extension.toLowerCase()] || defaultIcon;
+  };
+
+  const renderFileIcon = (file) => {
+    const extension = (file.name.split('.').pop() || '').toLowerCase();
+    const iconUrl = getFileIcon(extension);
+    return (
+      <div className='file-icon'>
+        <img src={iconUrl} alt={`${extension} icon`} style={{ width: '20px', height: '20px' }} />
+      </div>
+    );
+  };
+
+
   const [files, setFiles] = useState([]);
   const handleUpload = async (event) => {
     const items = event.target.files;
