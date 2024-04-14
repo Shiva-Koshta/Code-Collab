56db2cb9fa5343227383832f56b370952c1095c4
Anisha_Prakash <anishaprakashmbec@gmail.com>
2024-04-14 16:23:30 +0530

    Create App.test.js

---
+++ frontend/src/__test__/App.test.js
@@ -0,0 +1,52 @@
+import { render, screen } from '@testing-library/react';
+import axios from 'axios';
+import App from '../App';
+
+jest.mock('axios');
+
+describe('App', () => {
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    beforeEach(() => {
+    // Mocking the response of axios.get for successful user authentication
+    axios.get.mockResolvedValue({
+      data: {
+        user: {
+          _json: {
+            // Mock user data
+            name: 'Test User',
+            email: 'test@example.com'
+            // Add any other necessary user data here
+          }
+        }
+      }
+    });
+  });
+  afterEach(() => {
+    // Restore console.error to its original implementation after each test
+    consoleErrorSpy.mockRestore();
+  });
+  test('renders RoomCreation when user is logged in', async () => {
+    // Render the App component
+    render(<App />);
+
+    // Wait for the asynchronous useEffect hook to complete
+    await screen.findByText(/Create New Room/i);
+
+    // Assert that RoomCreation is rendered when user is logged in
+    expect(screen.getByText(/Create New Room/i)).toBeInTheDocument();
+  });
+
+  test('renders Login when user is not logged in', async () => {
+    // Mock axios.get to simulate user not being logged in
+    axios.get.mockRejectedValueOnce(new Error('Unauthorized'));
+
+    // Render the App component
+    render(<App />);
+
+    // Wait for the asynchronous useEffect hook to complete
+    await screen.findByText(/login/i);
+
+    // Assert that Login is rendered when user is not logged in
+    expect(screen.getByText(/login/i)).toBeInTheDocument();
+  });
+});
