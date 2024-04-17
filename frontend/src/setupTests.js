663a75b05fff57ffc3acaa1dc5c5f9f9546346a4
Anisha_Prakash <anishaprakashmbec@gmail.com>
2024-04-17 18:42:12 +0530

    removing node modules

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,17 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
-// Mock matchMedia
-window.matchMedia = jest.fn().mockImplementation(query => {
-    return {
-      matches: false, // You can adjust this value based on your needs
-      media: query,
-      onchange: null,
-      addListener: jest.fn(), // Deprecated
-      removeListener: jest.fn(), // Deprecated
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      dispatchEvent: jest.fn(),
-    };
-  });
-  
