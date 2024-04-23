b7303a94f513d480d7f6ba811de7a7ea1b1b3159
Komalpreet Singh <2021csb1237@iitrpr.ac.in>
2024-04-23 10:59:01 +0530

    completed the socket changes

--- frontend/src/pages/EditorPage.js
+++ frontend/src/pages/EditorPage.js
@@ -65,9 +65,8 @@ const EditorPage = () => {
   // const [menuOpen, setMenuOpen] = useState({})
 
   const toggleLeftDiv = () => {
-    setIsLeftDivOpen(prevState => !prevState)
-  }
-
+    setIsLeftDivOpen((prevState) => !prevState);
+  };
 
   // const handleUserMenuToggle = (username) => {
   //   setMenuOpen(prevMenuOpen => ({
@@ -497,36 +496,46 @@ const EditorPage = () => {
         {<Toaster position="top-center" reverseOrder={false} />}
 
         {/* {isLeftDivOpen && ( */}
-        <div style={{width: "300px", position:"fixed", left:"0", top:"0"}}>
-        <Sidebar
-          contentChanged={contentChanged}
-          setContentChanged={setContentChanged}
-          fileContent={fileContent}
-          setFileContent={setFileContent}
-          editorRef={editorRef}
-          // isConnectedComponentOpen={isConnectedComponentOpen}
-          // handleToggle={handleToggle}
-          connectedUsers={connectedUsers}
-          toggleChat={toggleChat}
-          unreadMessages={unreadMessages}
-          // copyRoomId={copyRoomId}
-          // leaveRoom={leaveRoom}
-          roomId={roomId}
-          isLeftDivOpen={isLeftDivOpen}
-          toggleLeftDiv={toggleLeftDiv}
-          leftIcon={leftIcon}
-          storedUserData={storedUserData}
-          host={host}
-          connectedUserRoles={connectedUserRoles}
-          setConnectedUserRoles={setConnectedUserRoles}
-          socketRef={socketRef}
-        />
-
+        <div style={{ width: "300px", position: "fixed", left: "0", top: "0" }}>
+          <Sidebar
+            contentChanged={contentChanged}
+            setContentChanged={setContentChanged}
+            fileContent={fileContent}
+            setFileContent={setFileContent}
+            editorRef={editorRef}
+            // isConnectedComponentOpen={isConnectedComponentOpen}
+            // handleToggle={handleToggle}
+            connectedUsers={connectedUsers}
+            toggleChat={toggleChat}
+            unreadMessages={unreadMessages}
+            // copyRoomId={copyRoomId}
+            // leaveRoom={leaveRoom}
+            roomId={roomId}
+            isLeftDivOpen={isLeftDivOpen}
+            toggleLeftDiv={toggleLeftDiv}
+            leftIcon={leftIcon}
+            storedUserData={storedUserData}
+            host={host}
+            connectedUserRoles={connectedUserRoles}
+            setConnectedUserRoles={setConnectedUserRoles}
+            socketRef={socketRef}
+          />
         </div>
-        
+
         <div
           className={`overflow-y-scroll overflow-x-scroll transition-all duration-500 ease-in-out absolute top-0 `}
-          style={{ width: isChatOpen ? (isLeftDivOpen ? `calc(100% - 592px)` : `calc(100% - 320px)`) : (isLeftDivOpen ? `calc(100% - 300px)` : `calc(100%)`),  left: isLeftDivOpen ?"300px": "0px", overflowX: "scroll", position:"fixed" }}
+          style={{
+            width: isChatOpen
+              ? isLeftDivOpen
+                ? `calc(100% - 592px)`
+                : `calc(100% - 320px)`
+              : isLeftDivOpen
+              ? `calc(100% - 300px)`
+              : `calc(100%)`,
+            left: isLeftDivOpen ? "300px" : "0px",
+            overflowX: "scroll",
+            position: "fixed",
+          }}
         >
           <Editor
             // handleDownloadFile={handleDownloadFile}
@@ -539,7 +548,10 @@ const EditorPage = () => {
             connectedClients={connectedUsernamesRef}
           />
           {!isLeftDivOpen && (
-            <div style={{zIndex: "9999"}} className="absolute left-0 top-1/2 transform transition duration-500 hover:animate-bounce-right">
+            <div
+              style={{ zIndex: "9999" }}
+              className="absolute left-0 top-1/2 transform transition duration-500 hover:animate-bounce-right"
+            >
               <button className="text-white" onClick={toggleLeftDiv}>
                 {leftIcon}
               </button>
@@ -559,7 +571,7 @@ const EditorPage = () => {
         />
 
         {isChatOpen && (
-          <div style={{backgroundColor: "#1c1e29"}}>
+          <div style={{ backgroundColor: "#1c1e29" }}>
             <Chat
               setIsChatOpen={setIsChatOpen}
               messages={messages}
