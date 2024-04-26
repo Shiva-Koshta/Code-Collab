1a902252018689d044d9950128d35a874d988a41
kceeprime <117650081+kceeprime@users.noreply.github.com>
2024-04-26 15:26:52 +0530

    moved endpoints.js into routes

--- backend/server.js
+++ backend/server.js
@@ -164,7 +164,7 @@ io.on("connection", (socket) => {
 });
 
 module.exports = { io, server, http };
-const endpoints = require("./endpoints");
+const endpoints = require("./routes/endpoints");
 app.use(endpoints);
 
 // Connect to MongoDB
